a3f3e64ad3040eb56a925ff5e943e95f13cc1175
pommee <hugoh@hotmail.se>
2023-10-10 15:43:56 +0200

    fix button for faq

--- js/faq.js
+++ js/faq.js
@@ -3,10 +3,16 @@ let faqWindow;
 function questionsAndAnswers() {
   let questionMark = document.querySelector(".fa-circle-question");
   faqWindow = document.querySelector("#faq-window");
+  closeFaqWindowsBtn = document.querySelector("#close-faq-window-btn");
 
   questionMark.addEventListener("click", function () {
     faqWindow.classList = "visible";
     document.querySelector("#main-content").style = "filter: blur(2px);";
+  });
+
+  closeFaqWindowsBtn.addEventListener("click", function () {
+    faqWindow.classList = "invinsible";
+    document.querySelector("#main-content").style = "filter: blur(0px);";
   });
 
   fetch("faq.json")
@@ -14,28 +20,21 @@ function questionsAndAnswers() {
     .then((data) => {
       const container = document.getElementById("faq-window");
 
-      // Loop through the JSON data and create cards
       data.forEach((item) => {
-        // Create a card div
         const card = document.createElement("div");
         card.classList.add("card");
 
-        // Create elements for the question and answer
         const questionElement = document.createElement("h3");
         questionElement.textContent = item.question;
 
-        // Create a paragraph element for the answer
         const answerElement = document.createElement("p");
 
-        // Replace "\n" with <br> tags for newline
         const answerText = item.answer.replace(/\n/g, "<br>");
         answerElement.innerHTML = answerText;
 
-        // Append elements to the card
         card.appendChild(questionElement);
         card.appendChild(answerElement);
 
-        // Append the card to the container
         container.appendChild(card);
       });
     })
